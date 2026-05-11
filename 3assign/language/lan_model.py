8f784c3945bddbd54c1fbf36f4a8e14aad2595fd
carlamanterola <carla.m.cuadra@opendeusto.es>
2026-05-11 13:12:08 +0200

    A3 Initial Commit

---
+++ 3assign/language/lan_model.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class SentimentLSTM(nn.Module):
+    def __init__(self, vocab_size, embed_dim, hidden_dim):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.lstm = nn.LSTM(embed_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        _, (h_n, _) = self.lstm(x)   # many-to-one: only last hidden state
+        return self.fc(h_n[-1])
---
+++ 3assign/main.py
@@ -0,0 +1,28 @@
+from datasets import load_dataset
+
+'''
+imdb dataset: https://huggingface.co/datasets/stanfordnlp/imdb
+
+Two variables:
+- text: the movie review
+- label: the sentiment (0 for negative, 1 for positive)
+'''
+ds = load_dataset("stanfordnlp/imdb")
+
+# ===========================================================================
+# TASK 1: LANGUAGE - MOVIE REVIEW CLASSIFICATION
+# ===========================================================================
+print("=" * 80)
+print("TASK 1: LANGUAGE - MOVIE REVIEW CLASSIFICATION")
+print("=" * 80)
+
+
+
+
+
+# ===========================================================================
+# TASK 2: TIME-SERIES
+# ===========================================================================
+print("=" * 80)
+print("TASK 1: TIME-SERIES - ")
+print("=" * 80)
